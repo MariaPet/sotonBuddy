9cd1a43dece31f8b966c1abae92f59ff35e2b61f
mariapetriti <petriti.maria@gmail.com>
2018-04-20 16:48:49 +0300

    index home endpoint

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const request = require('request');
+
+app.set('port', (process.env.port || 5000));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('hello');
+})
+
