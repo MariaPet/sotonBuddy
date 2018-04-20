bfb8b82c5ad474d872e2461b11bbecaba6993d65
mariapetriti <petriti.maria@gmail.com>
2018-04-20 17:27:17 +0300

    typo env var name

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const request = require('request');
 
-app.set('port', (process.env.port || 5000));
+app.set('port', (process.env.PORT || 5000));
 app.use(bodyParser.json());
 
 
