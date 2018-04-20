eb463ac0f58522e301eb16900f69ee0f9988f73a
mariapetriti <petriti.maria@gmail.com>
2018-04-20 17:07:18 +0300

    app listen to port

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const request = require('request');
 
 app.set('port', (process.env.port || 5000));
 app.use(bodyParser.json());
+app.listen(app.get('port'), () => console.log('App is listening to ' + app.get('port')));
 
 app.get('/', (req, res) => {
     res.send('Hello automatic deployment');
