d34c5f31f4935f7f8bc79ce567bf2c57e697154d
mariapetriti <petriti.maria@gmail.com>
2018-04-20 17:15:52 +0300

    check automatic heroku build

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const request = require('request');
 
 app.set('port', (process.env.port || 5000));
 app.use(bodyParser.json());
-app.listen(app.get('port'), () => console.log('App is listening to ' + app.get('port')));
+
 
 app.get('/', (req, res) => {
     res.send('Hello automatic deployment');
 })
+
+app.listen(app.get('port'), () => console.log('App is listening to ' + app.get('port')));
 
