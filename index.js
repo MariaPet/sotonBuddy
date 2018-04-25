ebd57d9ab961b97ccdd864cb8d816ceff69f1715
mariapetriti <petriti.maria@gmail.com>
2018-04-25 17:29:24 +0300

    fix function name

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.post('/webhook', (req, res) => {
             // will only ever contain one message, so we get index 0
             let webhook_event = entry.messaging[0];
             let sender = webhook_event.sender.id;
-            sendTextMessage(sender,"Hello buddy!");
+            sendMessage(sender,"Hello buddy!");
             console.log('Message response')
             res.status(200).send('EVENT_RECEIVED');
         });
