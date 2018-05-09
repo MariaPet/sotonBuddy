66b8cb673fbad654108555336dc2852f3fd5bbaa
mariapetriti <petriti.maria@gmail.com>
2018-05-09 17:02:18 +0300

    explore rdf

--- index.js
+++ index.js
@@ -98,12 +98,14 @@ app.post('/webhook', (req, res) => {
                             console.log("Oops, something happened and couldn't fetch data");
                         } else {
                             // do something with the data in the store
-                            console.log(xhr)
+                            // console.log(xhr)
                             var mimeType = 'application/rdf+xml'
                             var menuUri = 'http://id.southampton.ac.uk/dataset/catering-daily-menu'
                             try {
                                 // $rdf.parse(xhr, store, menuUri, mimeType)
+                                var me = $rdf.sym('http://data.southampton.ac.uk/dumps/catering-daily-menu/2018-05-09/catering-daily-menu.nt');
                                 console.log(store.statements) // shows the parsed statements
+                                console.log(me)
                             } catch (err) {
                                 console.log(err)
                             }
