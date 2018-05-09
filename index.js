aef875e0c0df149a83ad3c7299e9cf0b64b815c9
mariapetriti <petriti.maria@gmail.com>
2018-05-10 01:54:28 +0300

    remove bs

--- index.js
+++ index.js
@@ -97,9 +97,9 @@ app.post('/webhook', (req, res) => {
                     var requestedMenu = events.whichMenuPostback(webhookEvent)
                     if (requestedMenu) {
                         // fetcher.nowOrWhenFetched(cateringUrl, function(ok, body, xhr) {
-                            if (!ok) {
-                                console.log("Oops, something happened and couldn't fetch data");
-                            } else {
+                            // if (!ok) {
+                                // console.log("Oops, something happened and couldn't fetch data");
+                            // } else {
                                 try {
                                     var RDF = $rdf.Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#")
                                     var RDFS = $rdf.Namespace("http://www.w3.org/2000/01/rdf-schema#")
@@ -140,7 +140,7 @@ app.post('/webhook', (req, res) => {
                                 catch (err) {
                                     console.log(err)
                                 }
-                            }
+                            // }
                         // });
                     }
                     
