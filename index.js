22a82a9042ca8f5cf386fdfe011401e840556e59
mariapetriti <petriti.maria@gmail.com>
2018-05-10 16:19:17 +0300

    find message type

--- index.js
+++ index.js
@@ -48,6 +48,7 @@ app.post('/webhook', (req, res) => {
             // Gets the message. entry.messaging is an array, but 
             // will only ever contain one message, so we get index 0
             let webhookEvent = entry.messaging[0];
+            console.log(JSON.stringify(webhookEvent))
             let sender = webhookEvent.sender.id;
             if (events.isStartPostback(webhookEvent)) {
                 sendMessage(sender,"Hello buddy! Choose an option from the menu.");
